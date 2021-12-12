78e278e25f9aa78cca7f73488e6d25e31378cfff
maedhays <maedhays@iu.edu>
2021-12-12 11:02:51 -0500

    Adding shiny and datasets

---
+++ R/shinyapplication.R
@@ -0,0 +1,75 @@
+# library(shiny)
+# library(ggplot2)
+# library(dplyr)
+# library(nycflights13)
+# library(shinyWidgets)
+# 
+# 
+# dframe <- ind(teststroke)
+# 
+# ui <- fluidPage(fluidRow(
+#   column(12,
+#          dataTableOutput('table')
+#   )
+# ),
+#   
+#   sidebarLayout(
+#     
+#     # Inputs
+#     sidebarPanel(
+#       selectInput(inputId = "by", 
+#                   label = "Search By:",
+#                   choices = list("Individual", "Variable", "Everyone"),
+#                   selected = "Everyone"),
+#       conditionalPanel("input.by == 'Individual'",
+#                        selectInput(inputId = 'id',
+#                   label = 'Select an ID',
+#                   choices = dframe$individual)),
+#       conditionalPanel("input.by == 'Variable'",
+#                        checkboxGroupInput("checkGroup", 
+#                                           label = h3("Variables"), 
+#                          choices = names(dframe),
+#                          selected = c("hypertension", "bmi"))),
+#       conditionalPanel("input.by == 'Everyone'", 
+#                        sliderInput(inputId = 'nmiss',
+#                   label = 'Minimum Number of Missing Values',
+#                   min = 1,
+#                   max = ncol(dframe),
+#                   value = 1, step = 1))
+#     ),
+#     
+#     # Outputs
+#     mainPanel(
+#       plotOutput('df'),
+# 
+#     )
+#   )
+# )
+# 
+# # Server
+# server <- function(input, output) {
+#   
+#   
+#   dat <- reactive({
+#     if (input$by =="Individual")
+#     {
+#       l <- is.na(dframe[input$id,])
+#       l[1] <- TRUE
+#       dframe[input$id,l] 
+#     } 
+#     else if (input$by == "Variable"){
+#       dframe %>% missvar(input$checkGroup)
+#     }
+#     else {
+#       dframe %>% missoutput(input$nmiss)
+#     }
+#   })
+#   
+#   output$table <- renderDataTable(dat())
+# 
+#   
+# }
+# 
+# # Create a Shiny app object
+# shinyApp(ui = ui, server = server)
+# 
---
+++ teststroke.R
@@ -0,0 +1,40 @@
+ï..id gender age hypertension heart_disease ever_married work_type Residence_type avg_glucose_level bmi smoking_status stroke
+1 9046 Male 67 0 1 Yes Private Urban 228.69 36.6 formerly smoked 1
+2 51676 Female 61 NA 0 NA Self-employed Rural 202.21 NA NA 1
+3 31112 Male 80 0 1 Yes Private Rural 105.92 32.5 never smoked 1
+4 60182 Female 49 0 0 Yes Private Urban 171.23 34.4 smokes 1
+5 1665 NA 79 NA NA Yes NA NA NA NA never smoked 1
+6 56669 Male 81 0 0 Yes Private Urban 186.21 29 formerly smoked 1
+7 53882 Male 74 1 1 NA NA NA 70.09 27.4 never smoked 1
+8 10434 Female 69 0 0 No Private Urban 94.39 22.8 never smoked 1
+9 27419 Female 59 0 0 Yes Private Rural 76.15 NA Unknown 1
+10 60491 Female 78 0 0 Yes Private Urban 58.57 24.2 Unknown 1
+11 12109 Female 81 1 0 Yes Private Rural 80.43 29.7 never smoked 1
+12 12095 Female 61 0 1 Yes Govt_job Rural 120.46 36.8 smokes 1
+13 12175 Female 54 0 0 Yes Private Urban 104.51 27.3 smokes 1
+14 8213 Male 78 0 1 Yes Private Urban 219.84 NA Unknown 1
+15 5317 Female 79 0 1 Yes Private Urban 214.09 28.2 never smoked 1
+16 58202 NA NA NA NA NA NA NA NA NA NA NA
+17 56112 Male 64 0 1 Yes Private Urban 191.61 37.5 smokes 1
+18 34120 Male 75 1 0 Yes Private Urban 221.29 25.8 smokes 1
+19 27458 Female 60 0 0 No Private Urban 89.22 37.8 never smoked 1
+20 25226 Male 57 0 1 No Govt_job Urban 217.08 NA Unknown 1
+21 70630 Female 71 0 0 Yes Govt_job Rural 193.94 22.4 smokes 1
+22 13861 Female 52 1 0 Yes Self-employed Urban 233.29 48.9 never smoked 1
+23 68794 Female 79 0 0 Yes Self-employed Urban 228.7 26.6 never smoked 1
+24 64778 Male 82 NA 1 Yes Private Rural 208.3 32.5 Unknown 1
+25 4219 Male 71 0 0 Yes Private Urban 102.87 27.2 formerly smoked 1
+26 70822 Male 80 0 0 Yes Self-employed Rural 104.12 23.5 never smoked 1
+27 38047 Female 65 0 0 Yes Private Rural 100.98 28.2 formerly smoked 1
+28 61843 Male 58 0 0 Yes Private Rural 189.84 NA Unknown 1
+29 54827 Male 69 0 1 Yes Self-employed Urban NA 28.3 NA NA
+30 69160 Male 59 0 0 Yes Private Rural 211.78 NA formerly smoked 1
+31 43717 Male 57 1 0 Yes Private Urban 212.08 44.2 smokes 1
+32 33879 Male 42 NA NA NA NA NA 83.41 25.4 Unknown 1
+33 39373 Female 82 1 0 Yes Self-employed Urban 196.92 22.2 never smoked 1
+34 54401 Male 80 0 1 Yes Self-employed Urban 252.72 30.5 formerly smoked 1
+35 14248 Male 48 0 0 No Govt_job Urban 84.2 29.7 never smoked 1
+36 712 Female 82 1 1 No Private Rural 84.03 26.5 formerly smoked 1
+37 47269 Male 74 0 0 Yes Private Rural 219.72 33.7 formerly smoked 1
+38 24977 Female 72 1 0 Yes Private Rural 74.63 23.1 formerly smoked 1
+39 47306 Male 58 0 0 No Private Rural 92.62 32 Unknown 1
